d803c79f1d5c2695ad0981867a04a560310c82d4
John Gruska <jag47@cornell.edu>
2016-04-22 14:00:35 -0400

    User model test ready

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ gulp.task('buildJS', ['lintJS'], function () {
 
 gulp.task('testServerJS', function () {
     require('babel-register');
-	return gulp.src('./tests/server/**/*.js', {
+    return gulp.src('./tests/server/**/*.js', {
 		read: false
 	}).pipe(mocha({ reporter: 'spec' }));
 });
