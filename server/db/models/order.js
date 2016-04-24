02960de75fd854dd6ab57340e8d4df95fd9d3362
Saif Tase <saiftase@gmail.com>
2016-04-24 08:49:03 -0400

    Updated Order model/route

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -31,10 +31,21 @@ var schema = new mongoose.Schema({
     dateCanceled: Date
 });
 
-schema.methods.timestampStatus = function(status){
+schema.methods.timestampStatus = function(){
     //Test - What happens if field doesn't exist on model?
-    var field = "date" + status.charAt(0).toUpperCase() + status.slice(1);
+    var field = "date" + this.status.charAt(0).toUpperCase() + this.status.slice(1);
     this[field] = Date.now();
+    return this;
 }
+
+schema.pre('save', function (next) {
+
+    if(this.isNew){
+        this.dateCreated = Date.now();
+    }
+
+    next();
+
+});
 
 module.exports = mongoose.model('Order', schema);
