fcf35cd6503b8a691739a555ef4116f54779e5f3
Christopher Kennedy <cjkb90@users.noreply.github.com>
2016-04-24 20:08:00 -0400

    Fixed review routes

--- tests/server/models/review-test.js
+++ tests/server/models/review-test.js
@@ -4,6 +4,47 @@ var clearDB = require('mocha-mongoose')(dbURI);
 var expect = require('chai').expect;
 var mongoose = require('mongoose');
 var Review = require('../../../server/db/models/review');
+var Product = require('../../../server/db/models/product');
+var Category = require('../../../server/db/models/category');
+var User = require('../../../server/db/models/user');
+
+var generateNewUser = function(){
+	return new User({
+		email: 'chris@gmail.com',
+		password: 'chrisrules',
+		firstName: 'Chris',
+		lastName: 'Topholus',
+		role: 'User',
+		active: true,
+		pendingPasswordReset: false
+	})
+}
+
+var generateNewCategory = function(){
+	return new Category({
+		name: 'Cheap'
+	});
+}
+
+var generateNewProduct = function(){
+	return new Product({
+		title: 'Cool Thing',
+		categories: [generateNewCategory()],
+		price: 2,
+		inventoryQty: 50,
+		active: true
+	});
+}
+
+var generateNewReview = function(){
+	return new Review({
+		product: generateNewProduct()._id,
+		user: generateNewUser()._id,
+		title: 'Funky Title',
+		stars: 5,
+		description: 'I liked this product'
+	});
+}
 
 describe('Review', function(){
 	beforeEach('Establish DB connection', function (done) {
@@ -12,30 +53,27 @@ describe('Review', function(){
         done();
     });
 
-	it('has productId, userId, title, stars, and description', function(done){
-		var review = new Review({
-			productId: '123',
-			userId: '456',
-			title: 'Funky Title',
-			stars: 5,
-			description: 'I liked this product'
-		});
+	it('has product, user, title, stars, and description', function(done){
+		var review = generateNewReview();
 
 		review.save()
 		.then(function(savedReview){
-			expect(savedReview.productId).to.equal('123');
-			expect(savedReview.userId).to.equal('456');
+			expect(savedReview.product).to.equal(review.product);
+			expect(savedReview.user).to.equal(review.user);
 			expect(savedReview.title).to.equal('Funky Title');
 			expect(savedReview.stars).to.equal(5);
 			expect(savedReview.description).to.equal('I liked this product');
 			done();
-		});
+		})
+		.catch(function(err){
+			done(err);
+		})
 	});
 
-	it('requires productId', function(done){
+	it('requires product', function(done){
 		var review = new Review({
-			//productId: '123',
-			userId: '456',
+			//product: '123',
+			user: generateNewUser()._id,
 			title: 'Funky Title',
 			stars: 5,
 			description: 'I liked this product'
@@ -45,13 +83,13 @@ describe('Review', function(){
 			expect(err).to.be.an('object');
 			expect(err.message).to.exist;
 			done();
-		}).then(null, done);
+		})
 	});
 
-	xit('requires userId', function(done){
+	it('requires user', function(done){
 		var review = new Review({
-			productId: '123',
-			// userId: '456',
+			product: generateNewProduct()._id,
+			// user: '456',
 			title: 'Funky Title',
 			stars: 5,
 			description: 'I liked this product'
@@ -61,13 +99,13 @@ describe('Review', function(){
 			expect(err).to.be.an('object');
 			expect(err.message).to.exist;
 			done();
-		}).then(null, done);
+		})
 	});
 
-	xit('requires title', function(done){
+	it('requires title', function(done){
 		var review = new Review({
-			productId: '123',
-			userId: '456',
+			product: generateNewProduct()._id,
+			user: generateNewUser()._id,
 			// title: 'Funky Title',
 			stars: 5,
 			description: 'I liked this product'
@@ -77,13 +115,13 @@ describe('Review', function(){
 			expect(err).to.be.an('object');
 			expect(err.message).to.exist;
 			done();
-		}).then(null, done);
+		})
 	});
 
-	xit('stars', function(done){
+	it('stars', function(done){
 		var review = new Review({
-			productId: '123',
-			userId: '456',
+			product: generateNewProduct()._id,
+			user: generateNewUser()._id,
 			title: 'Funky Title',
 			// stars: 5,
 			description: 'I liked this product'
@@ -93,13 +131,13 @@ describe('Review', function(){
 			expect(err).to.be.an('object');
 			expect(err.message).to.exist;
 			done();
-		}).then(null, done);
+		})
 	});
 
-	xit('requires description', function(done){
+	it('requires description', function(done){
 		var review = new Review({
-			productId: '123',
-			userId: '456',
+			product: generateNewProduct()._id,
+			user: generateNewUser()._id,
 			title: 'Funky Title',
 			stars: 5
 			// description: 'I liked this product'
@@ -109,13 +147,13 @@ describe('Review', function(){
 			expect(err).to.be.an('object');
 			expect(err.message).to.exist;
 			done();
-		}).then(null, done);
+		})
 	});
 
-	xit('requires a star value of at least 1', function(done){
+	it('requires a star value of at least 1', function(done){
 		var review = new Review({
-			productId: '123',
-			userId: '456',
+			product: generateNewProduct()._id,
+			user: generateNewUser()._id,
 			title: 'Funky Title',
 			stars: 0,
 			description: 'I liked this product'
@@ -125,13 +163,13 @@ describe('Review', function(){
 			expect(err).to.be.an('object');
 			expect(err.message).to.exist;
 			done();
-		}).then(null, done);
+		})
 	});
 
-	xit('requires a star value of at most 5', function(done){
+	it('requires a star value of at most 5', function(done){
 		var review = new Review({
-			productId: '123',
-			userId: '456',
+			product: generateNewProduct()._id,
+			user: generateNewUser()._id,
 			title: 'Funky Title',
 			stars: 6,
 			description: 'I liked this product'
@@ -141,7 +179,7 @@ describe('Review', function(){
 			expect(err).to.be.an('object');
 			expect(err.message).to.exist;
 			done();
-		}).then(null, done);
+		})
 	});
 });
 
