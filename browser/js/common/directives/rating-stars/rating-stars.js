7194731238f34786db267c8fccf97d199ed4ec2b
John Gruska <johngruska@Johns-MacBook-Pro-3.local>
2016-04-30 10:15:30 -0400

    Still building...

--- browser/js/common/directives/rating-stars/rating-stars.js
+++ browser/js/common/directives/rating-stars/rating-stars.js
@@ -1,11 +1,22 @@
 app.directive('ratingStars', function () {
     return {
         restrict: 'E',
+        scope: {
+        	size: '@'
+        },
         templateUrl: 'js/common/directives/rating-stars/rating-stars.html',
         link: function(scope) {
+        	if(!scope.size)
+        		scope.size = 'xl';
+        	
+        	//scope.id = 'input-'+Math.random().toString(36).slice(3,10);
+        	scope.id = 'input-id';
+        	console.log(scope.id);
+        	
         	$("#input-id").rating({
         		showCaption: false,
-        		clearButton: ''
+        		clearButton: '',
+        		size: scope.size
         	});
         }
     };
