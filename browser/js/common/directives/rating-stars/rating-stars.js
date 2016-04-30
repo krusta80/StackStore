d7414309fe83beb0a146c9d243bf47b2a3ff9a96
John Gruska <jag47@cornell.edu>
2016-04-30 10:52:58 -0400

    Added update one-way bind and funtionality

--- browser/js/common/directives/rating-stars/rating-stars.js
+++ browser/js/common/directives/rating-stars/rating-stars.js
@@ -1,23 +1,36 @@
 app.directive('ratingStars', function () {
+    /**
+		Attributes for this directive:
+		 - size (xs, sm, md, lg, xl)
+		 - updater (a function that will be passed the updated number of stars selected)
+
+		Example markup usage:
+		 <rating-stars size="xl" updater="updateReview(<review id>, stars)">
+
+		 The "stars" variable used in the above example is created by the directive and therefore
+		 cannot be substituted with any other variable name.
+    */
+
     return {
         restrict: 'E',
         scope: {
-        	size: '@'
+        	size: '@',
+        	updater: '&'
         },
         templateUrl: 'js/common/directives/rating-stars/rating-stars.html',
         link: function(scope) {
         	if(!scope.size)
-        		scope.size = 'xl';
+        		scope.size = 'lg';
         	
-        	//scope.id = 'input-'+Math.random().toString(36).slice(3,10);
-        	scope.id = 'input-id';
-        	console.log(scope.id);
+        	scope.id = 'input-'+Math.random().toString(36).slice(3,10);
         	
-        	$("#input-id").rating({
-        		showCaption: false,
-        		clearButton: '',
-        		size: scope.size
-        	});
+        	setTimeout(function() {
+	        	$("#"+scope.id).rating({
+	        		showCaption: false,
+	        		clearButton: '',
+	        		size: scope.size.toLowerCase()
+	        	});
+        	}, 0);
         }
     };
 });
