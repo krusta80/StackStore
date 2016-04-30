6c15404949ebd4404c97ce03a69cfd350f8f0dfa
John Gruska <jag47@cornell.edu>
2016-04-30 12:16:45 -0400

    Stars directive complete.

--- browser/js/common/directives/rating-stars/rating-stars.js
+++ browser/js/common/directives/rating-stars/rating-stars.js
@@ -3,6 +3,10 @@ app.directive('ratingStars', function () {
 		Attributes for this directive:
 		 - size (xs, sm, md, lg, xl)
 		 - updater (a function that will be passed the updated number of stars selected)
+		 - total-stars (numeric value, representing the number of stars for this rating -- default is 5)
+		 - filled-stars (numeric value, representing the number of filled stars for this rating -- default is 0)
+		 - display-only (default is false)
+		 - step (star increment size -- default is 1)
 
 		Example markup usage:
 		 <rating-stars size="xl" updater="updateReview(<review id>, stars)">
@@ -15,22 +19,44 @@ app.directive('ratingStars', function () {
         restrict: 'E',
         scope: {
         	size: '@',
-        	updater: '&'
+        	updater: '&',
+        	totalStars: '@',
+        	filledStars: '@',
+        	displayOnly: '@',
+        	step: '@'
         },
         templateUrl: 'js/common/directives/rating-stars/rating-stars.html',
         link: function(scope) {
+        	
+        	if(!scope.step)
+        		scope.step = 1;
         	if(!scope.size)
         		scope.size = 'lg';
+        	if(!scope.totalStars)
+        		scope.totalStars = 5;
+        	if(!scope.filledStars)
+        		scope.filledStars = 0;
+        	if(!scope.displayOnly || scope.displayOnly === 'false')
+        		scope.displayOnly = false;
         	
+        	scope.stars = scope.filledStars;
         	scope.id = 'input-'+Math.random().toString(36).slice(3,10);
+
+        	console.log(scope);
         	
+        	var settings = {
+        		showCaption: false,
+        		clearButton: '',
+        		size: scope.size.toLowerCase(),
+        		stars: scope.totalStars,
+        		displayOnly: scope.displayOnly,
+        		step: scope.step
+        	};
+
         	setTimeout(function() {
-	        	$("#"+scope.id).rating({
-	        		showCaption: false,
-	        		clearButton: '',
-	        		size: scope.size.toLowerCase()
-	        	});
-        	}, 0);
+	        	console.log(this);
+	        	$("#"+scope.id).rating(this);
+        	}.bind(settings), 0);
         }
     };
 });
