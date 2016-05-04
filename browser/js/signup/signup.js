e8a96c443c27283140d426ed76393fabba1a61c4
John Gruska <jag47@cornell.edu>
2016-05-04 09:37:34 -0400

    Created signup state and controller and started creating form

---
+++ browser/js/signup/signup.js
@@ -0,0 +1,29 @@
+app.config(function ($stateProvider) {
+
+    $stateProvider.state('signup', {
+        url: '/signup',
+        templateUrl: 'js/signup/signup.html',
+        controller: 'SignupCtrl'
+    });
+
+});
+
+app.controller('SignupCtrl', function ($scope, AuthService, $state) {
+
+    $scope.login = {};
+    $scope.error = null;
+
+    $scope.sendLogin = function (loginInfo) {
+
+        $scope.error = null;
+
+        AuthService.login(loginInfo).then(function () {
+            OrdersFactory.reloadCart();
+            $state.go('home');
+        }).catch(function () {
+            $scope.error = 'Invalid login credentials.';
+        });
+
+    };
+
+});
