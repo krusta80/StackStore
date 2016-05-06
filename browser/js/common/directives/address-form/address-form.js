37d5acd536d3f34f876f55f9f1a714f9abebe34f
Saif Tase <saiftase@gmail.com>
2016-05-06 05:10:29 -0400

    Full list of states to select from

--- browser/js/common/directives/address-form/address-form.js
+++ browser/js/common/directives/address-form/address-form.js
@@ -6,6 +6,9 @@ app.directive('addressForm', function ($state) {
         scope:{
         	values: "=",
         	checkbox: "="
+        },
+        link: function(scope){
+        	scope.states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
         }
     };
 
