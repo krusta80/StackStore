0d486a879d21634e81c29fc799581b98d5877a80
John Gruska <jag47@cornell.edu>
2016-05-08 03:24:05 -0400

    Made drone homepage video

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -9,10 +9,10 @@ app.config(function ($stateProvider) {
 app.controller('HomeCtrl', function($scope, $sce) {
 	
 	var videos = [
-		'blue.mp4',
-		'mix.mp4',
-		'http://ak9.picdn.net/shutterstock/videos/4405619/preview/stock-footage-moscow-jul-moving-camera-wide-view-timelapse-customers-buying-products-in-auchan-mall-on.mp4',
-		'http://i.istockimg.com/video_passthrough/79721929/153/79721929.mp4'
+		'stackstore.mp4',
+		// 'mix.mp4',
+		// 'http://ak9.picdn.net/shutterstock/videos/4405619/preview/stock-footage-moscow-jul-moving-camera-wide-view-timelapse-customers-buying-products-in-auchan-mall-on.mp4',
+		// 'http://i.istockimg.com/video_passthrough/79721929/153/79721929.mp4'
 	];
 
 	var getRandomVideo = function() {
