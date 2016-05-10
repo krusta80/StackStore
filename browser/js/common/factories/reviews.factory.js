4abf26a05203444196e6389ff83e02525cdb2afd
John Gruska <jag47@cornell.edu>
2016-05-10 00:35:32 -0400

    Branching enabled for products and reviews

--- browser/js/common/factories/reviews.factory.js
+++ browser/js/common/factories/reviews.factory.js
@@ -2,6 +2,14 @@ app.factory('ReviewsFactory', function($http){
 	var ReviewsFactory = {};
 
 	var review;
+	var reviewsCache = [];
+
+	var findIndex = function(items, newItem) {
+		for(var i = 0; i < items.length; i++)
+			if(items[i].origId === newItem.origId)
+				return i;
+		return -1;
+	}
 
 	ReviewsFactory.submitReview = function(review){
 		return $http.post('/api/reviews', review)
@@ -24,8 +32,13 @@ app.factory('ReviewsFactory', function($http){
 	}
 
 	ReviewsFactory.fetchAll = function(){
+		if(reviewsCache.length > 0) {
+			console.log("retrieving cached reviews...");
+			return reviewsCache;
+		}
 		return $http.get('/api/reviews')
 			.then(function(res){
+				reviewsCache = res.data;
 				return res.data;
 			})
 			.catch(function(err){
@@ -47,6 +60,10 @@ app.factory('ReviewsFactory', function($http){
 		console.log("submitting review...");
 		return $http.put('/api/reviews/'+review._id, review)
 			.then(function(res){
+				var index = findIndex(reviewsCache, res.data);
+				if(index > -1) 
+					reviewsCache[index] = res.data;
+					
 				return res.data;
 			})
 			.catch(function(err){
@@ -57,6 +74,9 @@ app.factory('ReviewsFactory', function($http){
 	ReviewsFactory.deleteReview = function(review){
 		return $http.delete('/api/reviews/'+review._id)
 			.then(function(res){
+				var index = findIndex(reviewsCache, res.data);
+				if(index > -1)
+					reviewsCache.splice(index,1);
 				return res.data;
 			})
 			.catch(function(err){
